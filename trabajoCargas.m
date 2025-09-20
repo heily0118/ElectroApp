fd14681fae6e7a66fc9ad4619902afb653327537
Heily Rios <heilyayala005@gmail.com>
2025-09-20 16:20:10 -0500

    Se modigo el trabajo cargas

--- trabajoCargas.m
+++ trabajoCargas.m
@@ -1,3 +1,17 @@
 function W = trabajoCargas(q1, q2, q3, a)
+    % trabajoCargas - Calcula el trabajo necesario para armar tres cargas
+    % en los vértices de un triángulo equilátero de lado a.
+    %
+    % Entradas:
+    %   q1, q2, q3 - cargas (Coulomb)
+    %   a - longitud del lado del triángulo (metros)
+    %
+    % Salida:
+    %   W - trabajo (Joules)
 
+    % Constante de Coulomb
+    k = 9e9; 
+
+    % Fórmula del trabajo en triángulo equilátero
+    W = k * (q1*q2 + q1*q3 + q2*q3) / a;
 end
